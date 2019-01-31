d86e3fdb9e874074c76cba0a3b9bfc3c6effa4c1
DESKTOP-04UD1B6\risha <rishabhnitc@gmail.com>
2019-01-31 22:51:34 +0100

    module 4

--- CityInfo.API/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,7 +22,7 @@ namespace CityInfo.API.Controllers
             return Ok(temp.PointsOfInterest);
         }
 
-        [HttpGet("{cityId}/pointsOfInterest/{id}")]
+        [HttpGet("{cityId}/pointsOfInterest/{id}", Name = "GetPointOfInterest")]
         public IActionResult GetPointsOfInterest(int cityId, int id)
         {
             var temp = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
@@ -32,11 +33,169 @@ namespace CityInfo.API.Controllers
 
             var pt = temp.PointsOfInterest.FirstOrDefault(p => p.Id == id);
 
-            if(pt == null)
+            if (pt == null)
             {
                 return NotFound();
             }
+
+
             return Ok(pt);
+        }
+
+        [HttpPost("{cityId}/pointsOfInterest")]
+        public IActionResult PointOfInterestCreation(int cityId, [FromBody] PointOfInterestCreationDto dto)
+        {
+            if (dto == null)
+            {
+                return BadRequest();
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+
+
+            if (dto.Name == dto.Description)
+            {
+                ModelState.AddModelError("Description", "Name and description cannot be same.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var maxPointOfInterest = CitiesDataStore.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+            var finalPointofInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterest,
+                Name = dto.Name,
+                Description = dto.Description
+            };
+            city.PointsOfInterest.Add(finalPointofInterest);
+
+            return CreatedAtRoute("GetPointOfInterest", new { cityId = cityId, id = finalPointofInterest.Id }, finalPointofInterest);
+        }
+
+        [HttpPut("{cityId}/pointsOfInterest/{id}")]
+        public IActionResult PointOfInterestUpdate(int cityId, int id, [FromBody] PointOfInterestUpdateDto dto)
+        {
+            if (dto == null)
+            {
+                return BadRequest();
+            }
+
+            if (dto.Name == dto.Description)
+            {
+                ModelState.AddModelError("Description", "Name and description cannot be same.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var poi = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (poi == null)
+            {
+                return NotFound();
+            }
+
+            poi.Name = dto.Name;
+            poi.Description = dto.Description;
+
+            return NoContent();
+        }
+
+        [HttpPut("{cityId}/pointsOfInterest/{id}")]
+        public IActionResult PointOfInterestPatch(int cityId, int id, [FromBody] JsonPatchDocument<PointOfInterestUpdateDto> patchDocument)
+        {
+            if (patchDocument == null)
+            {
+                return BadRequest();
+            }            
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var poi = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (poi == null)
+            {
+                return NotFound();
+            }
+
+            var poiForPatch = new PointOfInterestUpdateDto()
+            {
+                Name = poi.Name,
+                Description = poi.Description
+
+            };
+
+            patchDocument.ApplyTo(poiForPatch, ModelState);
+
+            // this one checks patch document ONLY
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (poiForPatch.Name == poiForPatch.Description)
+            {
+                ModelState.AddModelError("Description", "Name and description cannot be same.");
+            }
+
+            // this one patched POI document 
+            TryValidateModel(poiForPatch);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            poi.Name = poiForPatch.Name;
+            poi.Description = poiForPatch.Description;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{cityId}/pointsOfInterest/{id}")]
+        public IActionResult PointOfInterestDelete(int cityId, int id)
+        {
+          
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var poi = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (poi == null)
+            {
+                return NotFound();
+            }
+
+            var poiForPatch = new PointOfInterestUpdateDto()
+            {
+                Name = poi.Name,
+                Description = poi.Description
+
+            };
+
+            city.PointsOfInterest.Remove(poi);
+
+            return NoContent();
         }
 
     }
--- CityInfo.API/CityInfo.API/Models/PointOfInterestDto.cs
+++ CityInfo.API/CityInfo.API/Models/PointOfInterestDto.cs
@@ -1,9 +1,31 @@
-﻿namespace CityInfo.API
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API
 {
     internal class PointOfInterestDto
     {
         public int Id { get; internal set; }
         public string Description { get; internal set; }
         public string Name { get; internal set; }
+    }
+
+    public class PointOfInterestCreationDto
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Description { get;  set; }
+        [Required( ErrorMessage = "Name should be provided.")]
+        [MaxLength(50)]
+        public string Name { get;  set; }
+    }
+
+    public class PointOfInterestUpdateDto
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Name should be provided.")]
+        [MaxLength(50)]
+        public string Name { get; set; }
     }
 }
